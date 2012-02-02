12c8ed0027e1e56f6de7b5a68c8e1f276bc1a1ad
Olof Johansson <olof@oddhill.se>
2012-02-02 10:14:39 +0100

    Added settings summary to the settings form.

--- rabbit-hole.js
+++ rabbit-hole.js
@@ -2,20 +2,34 @@
 
 Drupal.behaviors.rabbitHole = {
   attach: function (context, settings) {
-    
-    var $redirectWrapper = $('fieldset.rabbit-hole-redirect-options');
+
+    // Set the summary for the settings form.
+    $('fieldset.rabbit-hole-settings-form').drupalSetSummary(function() {
+      var $rabbitHoleAction = $('#edit-rabbit-hole-action input:checked');
+      
+      // Get the label of the selected action.
+      var summary = $('label[for=' + $rabbitHoleAction.attr('id') + ']').text();
+      
+      // If redirect is selected, add the url to the summary.
+      if ($rabbitHoleAction.val() == settings.rabbitHole.redirectValue) {
+        summary += ' (' + $('#edit-rabbit-hole-redirect').val() + ')';
+      }
+      
+      return Drupal.checkPlain(summary);
+    });
   
     // Only show the redirect options if the user has selected redirect as the
     // behavior.
+    var $redirectFieldset = $('fieldset.rabbit-hole-redirect-options');
     if ($('#edit-rabbit-hole input[name=rabbit_hole_action]:checked').val() != settings.rabbitHole.redirectValue) {
-      $redirectWrapper.hide();
+      $redirectFieldset.hide();
     }
     $('#edit-rabbit-hole input[name=rabbit_hole_action]').change(function() {
       if ($(this).val() == settings.rabbitHole.redirectValue) {
-        $redirectWrapper.show();
+        $redirectFieldset.show();
       }
       else {
-        $redirectWrapper.hide();
+        $redirectFieldset.hide();
       }
     });
   
