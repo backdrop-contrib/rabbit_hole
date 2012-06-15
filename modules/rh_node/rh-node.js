a7e547da790043c35a7251105ef5a415dc549196
Olof Johansson <olof@oddhill.se>
2012-06-15 15:57:04 +0200

    Rabbit Hole 2.0
    This is a quite big rebuild of the structure of the module. The main module (Rabbit Hole) will act as a helper module for the other modules, and take care of the required base functionality.
    
    Other modules should be taking care of one entity each, e.g. rh_node and rh_taxonomy_term. Those modules will include one required hook called hook_rabbit_hole(), and will contain any specific behavior for their entity type.

---
+++ modules/rh_node/rh-node.js
@@ -0,0 +1,24 @@
+(function($) {
+
+Drupal.behaviors.rhNode = {
+  attach: function (context, settings) {
+
+    // Set the summary for the settings form.
+    $('fieldset.rabbit-hole-settings-form').drupalSetSummary(function() {
+      var $rabbitHoleAction = $('.rabbit-hole-action-setting input:checked');
+
+      // Get the label of the selected action.
+      var summary = $('label[for=' + $rabbitHoleAction.attr('id') + ']').text();
+      
+      // If redirect is selected, add the url to the summary.
+      if ($rabbitHoleAction.val() == settings.rabbitHole.redirectValue) {
+        summary += ' (' + $('.rabbit-hole-redirect-setting').val() + ')';
+      }
+      
+      return Drupal.checkPlain(summary);
+    });
+  
+  }
+}
+
+})(jQuery);
